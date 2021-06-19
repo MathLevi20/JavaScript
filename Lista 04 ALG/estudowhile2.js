c4a610bc22e140532946f96912f3e1bdf6c85f96
MathLevi20 <84115454+MathLevi20@users.noreply.github.com>
2021-06-19 00:37:31 -0300

    Add files via upload

---
+++ Lista 04 ALG/estudowhile2.js
@@ -0,0 +1,17 @@
+﻿const prompt = require('prompt-sync')()
+console.log("Bem vindo!!")
+
+function main() {
+    const Inicio = Number(prompt("Inicio:"))
+    const Fim = Number(prompt("Fim:"))
+    // Trabalho anterior
+    let atual = Inicio
+    while (atual <= Fim){ // condição de parada/continuidade   
+    //trabalho
+    if (atual%2!=0 && atual % 7 ==0)    
+    console.log(`${atual}`)
+        //convergencia de dados
+   atual =atual +1}
+}
+
+main()
