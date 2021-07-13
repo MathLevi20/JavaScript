f5b2091c01e97855bed44c0da96a84a71d22f272
Matheus Levi <84115454+MathLevi20@users.noreply.github.com>
2021-07-13 12:42:03 -0300

    Add files via upload

---
+++ Projetos/tesMINCOM02.js
@@ -0,0 +1,9 @@
+﻿const prompt = require('prompt-sync')()
+
+console.log("Bem vindo!!")
+
+const A = Number(prompt('Valor 01:')); 
+
+const B = -(2**(A-1))
+
+console.log('número de bits menor valor',B)
